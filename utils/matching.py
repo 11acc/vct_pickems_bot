ee1f0a075cdbc230958ee2dc1782bd8693e5dbda
11a <74672292+11acc@users.noreply.github.com>
2025-03-11 18:09:08 +0100

    error handling on fuzzy matching

--- utils/matching.py
+++ utils/matching.py
@@ -7,8 +7,10 @@ from db.db_instance import db
 
 
 # Fuzzy match the user input event to the best match in the db
-def find_best_event_match(input_event: str, year: int) -> any:
+def find_best_event_match(input_event: str, year: int) -> str | None:
     events_in_year = db.get_events_in_year(year)
+    if not events_in_year:
+        return None
     all_event_names = [row for ev in events_in_year for row in ev if row]
 
     best_match, score = process.extractOne(input_event, all_event_names)
