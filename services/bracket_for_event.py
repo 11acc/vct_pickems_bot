63b3206712d5056a87b7a3308469d8575e62b4bc
11a <74672292+11acc@users.noreply.github.com>
2025-05-16 13:46:23 +0200

    organising stuff

--- services/bracket_for_event.py
+++ services/bracket_for_event.py
@@ -15,25 +15,19 @@ def bracket_for_event(event_id: int, region: str = None, year: int = None) -> st
 
     # Generate bracket
     bracket_data = generate_bracket_config(event_id, region)
-
     import json
-    print(json.dumps(bracket_data, indent=4))
+    print(json.dumps(bracket_data, indent=2))
+    bracket_html = generate_bracket_html(bracket_data)
+
+    with open('bracket.html', 'w', encoding='utf-8') as f:
+        f.write(bracket_html)
 
 
-    # shorten team names to short names
-    # expand bracket item to more width, see which image looks best
-    # include icons inside each team
     # other votes go into vote-bottom
-
-
-
-    # bracket_html = generate_bracket_html(bracket_data)
 
     # no region also works
     # get vote data and modify html
 
-    # with open('bracket.html', 'w', encoding='utf-8') as f:
-    #     f.write(bracket_html)
 
     # call method to conver to image
     # return image path? upload url? ...?
